3a730da5eb331e580fc0b3ff1c2f36e07412da2a
Benny Dreamly <bgaming0277@gmail.com>
2024-12-09 19:56:42 -0700

    update to new unstable

--- manual_adventuresofdreamland_bennydreamly/hooks/Data.py
+++ manual_adventuresofdreamland_bennydreamly/hooks/Data.py
@@ -25,6 +25,12 @@ def after_load_region_file(region_table: dict) -> dict:
 def after_load_category_file(category_table: dict) -> dict:
     return category_table
 
+# called after the categories.json file has been loaded
+def after_load_option_file(option_table: dict) -> dict:
+    # option_table["core"] is the dictionary of modification of existing options
+    # option_table["user"] is the dictionary of custom options
+    return option_table
+
 # called after the meta.json file has been loaded and just before the properties of the apworld are defined. You can use this hook to change what is displayed on the webhost
 # for more info check https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/world%20api.md#webworld-class
 def after_load_meta_file(meta_table: dict) -> dict:
