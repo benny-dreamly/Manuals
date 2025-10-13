01f6e5d95a5eb411a1fba8d04392659ae7885e70
Benny Dreamly <bgaming0277@gmail.com>
2025-10-13 13:04:35 -0600

    port over the hook change

--- manual_adventuresofdreamland_bennydreamly/hooks/World.py
+++ manual_adventuresofdreamland_bennydreamly/hooks/World.py
@@ -71,7 +71,7 @@ def before_create_items_starting(item_pool: list, world: World, multiworld: Mult
 # The item pool after starting items are processed but before filler is added, in case you want to see the raw item pool at that stage
 def before_create_items_filler(item_pool: list, world: World, multiworld: MultiWorld, player: int) -> list:
     # Use this hook to remove items from the item pool
-    itemNamesToRemove: list[str] = [] # List of item names
+    itemNamesToRemove: list[str] = ["Progressive Command", "Progressive Command"] # List of item names
 
     # Add your code here to calculate which items to remove.
     #
